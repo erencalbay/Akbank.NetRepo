6acb07f7d6b453e3d6463f8fb8803508e8191a48
Eren Çalbay <erencalbayfb@hotmail.com>
2023-12-29 13:32:17 +0300

    Hafta 2.1: Bonus güncellemesi

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Models.Exceptions;
+using WebAPI.Repositories;
+using WebAPI.Services.Contracts;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+        private readonly ILoggerService _logger;
+
+        public AuthController(RepositoryContext context, ILoggerService logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] User user)
+        {
+            // user login control
+            if (user.Username == "erencalbay" && user.Password == "Admin")
+            {
+                return Ok(); //200
+            }
+            throw new BadRequestException("Username or password is wrong");
+        }
+    }
+}
